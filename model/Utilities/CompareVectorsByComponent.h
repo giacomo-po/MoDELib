dd228bc7bac4472a7b4513283a969e10356902c8
giacomo po <giacomopo@gmail.com>
2015-05-09 14:06:37 -0700

    fix bug in CompareVectorsByComponent affecting indentification of unique FEM nodes

--- model/Utilities/CompareVectorsByComponent.h
+++ model/Utilities/CompareVectorsByComponent.h
@@ -9,24 +9,49 @@
 #ifndef model_COMPAREVECTORSBYCOMPONENT_H_
 #define model_COMPAREVECTORSBYCOMPONENT_H_
 
+#include <math.h>       /* fabs */
 #include <Eigen/Dense>
+#include <model/Utilities/NumericPrecision.h>
 
 namespace model
 {
-	
-	template<typename T, unsigned int N, typename CastType=T>
-	struct CompareVectorsByComponent {
-		bool operator() (const Eigen::Matrix<T,N,1>& lhs, const Eigen::Matrix<T,N,1>& rhs) const{
-			return ( ((CastType) lhs(0))==((CastType) rhs(0)) )? CompareVectorsByComponent<T,N-1,CastType>()(lhs.template segment<N-1>(1),rhs.template segment<N-1>(1)) : (lhs(0)<rhs(0));
-		}
-	};
-	
-	template<typename T,typename CastType>
-	struct CompareVectorsByComponent<T,1,CastType> {
-		bool operator() (const Eigen::Matrix<T,1,1>& lhs, const Eigen::Matrix<T,1,1>& rhs) const{
-			return ((CastType) lhs(0))<((CastType) rhs(0));
-		}
-	};
+
+    template<typename T, unsigned int N, typename PrecisionType=T>
+    struct CompareVectorsByComponent
+    {
+        bool operator() (const Eigen::Matrix<T,N,1>& lhs, const Eigen::Matrix<T,N,1>& rhs) const
+        {
+            return ( fabs(lhs(0)-rhs(0))<=NumericPrecision<PrecisionType>::epsilon )? CompareVectorsByComponent<T,N-1,PrecisionType>()(lhs.template segment<N-1>(1),rhs.template segment<N-1>(1)) : (lhs(0)<rhs(0));
+        }
+    };
+    
+    template<typename T,typename PrecisionType>
+    struct CompareVectorsByComponent<T,1,PrecisionType>
+    {
+        bool operator() (const Eigen::Matrix<T,1,1>& lhs, const Eigen::Matrix<T,1,1>& rhs) const
+        {
+            return ( fabs(lhs(0)-rhs(0))<=NumericPrecision<PrecisionType>::epsilon )? false : (lhs(0)<rhs(0));
+        }
+    };
+
+    
+//	template<typename T, unsigned int N, typename CastType=T>
+//	struct CompareVectorsByComponent
+//    {
+//		bool operator() (const Eigen::Matrix<T,N,1>& lhs, const Eigen::Matrix<T,N,1>& rhs) const
+//        {
+//			return ( ((CastType) lhs(0))==((CastType) rhs(0)) )? CompareVectorsByComponent<T,N-1,CastType>()(lhs.template segment<N-1>(1),rhs.template segment<N-1>(1)) : (lhs(0)<rhs(0));
+//		}
+//	};
+//	
+//	template<typename T,typename CastType>
+//	struct CompareVectorsByComponent<T,1,CastType>
+//    {
+//		bool operator() (const Eigen::Matrix<T,1,1>& lhs, const Eigen::Matrix<T,1,1>& rhs) const
+//        {
+//			return ((CastType) lhs(0))<((CastType) rhs(0));
+//		}
+//	};
 		
 } // namespace model
 #endif
