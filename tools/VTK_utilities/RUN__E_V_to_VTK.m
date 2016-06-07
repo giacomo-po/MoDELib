17f6d3bd34f492717167cc9ca91e257a9e0e39dc
cube40 <stefan.sandfeld@fau.de>
2016-06-07 19:16:27 +0200

    cleanup

--- tools/VTK_utilities/RUN__E_V_to_VTK.m
+++ tools/VTK_utilities/RUN__E_V_to_VTK.m
@@ -1,14 +1,16 @@
 % polygons are numbered from 1...
 % vertex number are taken from the V file and +1 (Matlab one based)
+% TODO: replace 2-node polygons by only one polygon
 
-%% TODO: replace 2-node polygons by only one polygon
 clear all
 
 path_to_E_and_V = '.';
 step = 1;
 vtk_dir = '.';     
 
+% format of Burgers vector string
 bfmt = '%+06.3f %+06.3f %+06.3f';
+
 %% get unique Burgers vector list of all simulation steps
 idx = 1;
 for step = [0:10:1000]
@@ -16,13 +18,13 @@ for step = [0:10:1000]
     E=load(fullfile(path_to_E_and_V, ['/E/E_' num2str(step) '.txt']));
     for e=1:size(E,1)
         b=E(e,[3:5]);
-        bstring = sprintf(bfmt,b);
-        bb{idx} = [bstring];
+        b_list{idx} = sprintf(bfmt,b);
         idx = idx +1;
     end
 end
-bb = unique(bb,'rows');
+b_list = unique(b_list,'rows');
 
+%% store data from E and V file into vertex and polygon cell array
 for step = [0:10:1000]
     fprintf('writing VTK files, step=%i\n',step)
     % read vertex data
@@ -43,13 +45,9 @@ for step = [0:10:1000]
         vertex1 = round(E(e,2)) + 1;
         polygon{e}.vertices = [vertex0, vertex1];
         polygon{e}.nvertices = 2;
-        bstring = sprintf(bfmt,b);
-        polygon{e}.b_index = find(ismember(bb,bstring));
+        polygon{e}.b_index = find(ismember(b_list,sprintf(bfmt,b)));
     end
     
     vtk_file = sprintf('%03i_polygones.vtk',step);
     write_VTK_polygone_file( vtk_dir, vtk_file , polygon, vertex);
-
 end
-% 
-% find(norm(Burgers-b)<1e-6)
