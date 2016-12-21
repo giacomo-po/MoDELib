fe896ed4b3fa6e6b25145bc245c0b812b549fc87
gpo <giacomopo@gmail.com>
2016-12-20 23:48:02 -0800

    Rationalmatrix.h, testing CSL

--- model/LatticeMath/CSL.h
+++ model/LatticeMath/CSL.h
@@ -20,7 +20,11 @@
 
 namespace model
 {
-    
+    /*!Class template that computes the Coincident Site Lattice (CSL) of two 
+     * parent lattices using the Smith decomposition method [1].
+     *
+     * [1] Coincidence Lattices and Associated Shear Transformations
+     */
     template <int dim>
     class CSL : public Lattice<dim>
     {
@@ -30,7 +34,9 @@ namespace model
         typedef Eigen::Matrix<long int,dim,1> VectorDimI;
         typedef Eigen::Matrix<double,dim,dim> MatrixDimD;
         typedef Lattice<dim> LatticeType;
-        
+        typedef long long int IntValueType;
+        typedef Eigen::Matrix<IntValueType,dim,dim> MatrixInt;
+
         
         int _sigma;
 
@@ -52,9 +58,42 @@ namespace model
         /**********************************************************************/
         void update()
         {
-            RationalRotation<dim> rr(B.covBasis()*A.contraBasis().transpose());
+            // Suppose that lattice B is obtained from A through a rotaion R, that is B=R*A
+            // The rotation matrix is R=B*inv(A)
+            const MatrixDimD R(B.covBasis()*A.contraBasis().transpose());
+
+            // For the two lattices to have coincident sites, R must be a rational matrix
+            // Compute the integer matrix P and the integer sigma such that R=P/sigma
+            RationalRotation<dim> rr(R);
+            const MatrixInt& P=rr.integerMatrix();
             _sigma=rr.sigma();
-            SmithDecomposition<dim> sd(rr.integerMatrix());
+            
+            // The integer matrix P can be decomposed as P=X*D*Y using the Smith decomposition.
+            // X and Y are unimodular, and D is diagonal with D(k,k) dividing D(k+1,k+1)
+            // The decopmosition also computes the matices U and V such that D=U*P*V
+            SmithDecomposition<dim> sd(P);
+
+            // From R=B*inv(A)=P/sigma=X*D*Y/sigma=inv(U)*D*inv(V)/sigma, we have
+            // U*B*inv(A)*V=U*B*inv(A)*inv(Y)=(U*B)*inv(Y*A)=D/sigma
+            // Since U and Y are unimodular matrices, they define a change in basis,
+            // and therefore the bases B1=U*B and A1=Y*A satisfy
+            // B1*inv(A1)=D/sigma
+            // Since D is diagonal, B1 and A1 are simply scaled bases, with different
+            // scaling in different directions. Therefore their primitive vectors
+            // (columns of B1 and A1) satisfy
+            // sigma*b1_i=D(i,i)*a1_i
+            // Therefore, the primitive vectors of the CSL are
+            // c_i=sigma/gcd(sigma,D(i,i))*b1_i=D(i,i)/gcd(sigma,D(i,i))*a1_i
+            MatrixInt M(MatrixInt::Identity());
+            MatrixInt N(MatrixInt::Identity());
+            for(i=0;i<dim;++i)
+            {
+                const IntValueType& dii=sd.matrixD()(i,i);
+                M(i,i)=dii/gcd(_sigma,dii);
+                N(i,i)=_sigma/gcd(_sigma,dii);
+            }
+            std::cout<<(M*sd.matrixY()).template cast<double>()*A.covBasis()<<std::endl;
+            
         }
 
         /**********************************************************************/
