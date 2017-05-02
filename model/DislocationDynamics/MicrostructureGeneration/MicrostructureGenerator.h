7ba86eb7cdf3027fd60ef2fd105e514ab8b18db5
Yinan Cui <cuiyinan@g.ucla.edu>
2017-05-02 10:44:54 -0700

    microstructure: different size ditributions

--- model/DislocationDynamics/MicrostructureGeneration/MicrostructureGenerator.h
+++ model/DislocationDynamics/MicrostructureGeneration/MicrostructureGenerator.h
@@ -1,6 +1,7 @@
 /* This file is part of MODEL, the Mechanics Of Defect Evolution Library.
  *
  * Copyright (C) 2015 by Giacomo Po <gpo@ucla.edu>.
+ *                        Yinan Cui <cuiyinan@ucla.edu>
  *
  * model is distributed without any warranty under the
  * GNU General Public License (GPL) v2 <http://www.gnu.org/licenses/>.
@@ -17,6 +18,7 @@
 #include <model/DislocationDynamics/Materials/Material.h>
 #include <model/LatticeMath/LatticeVector.h>
 
+
 namespace model
 {
     
@@ -32,7 +34,15 @@ namespace model
 
         std::mt19937 generator;
         std::uniform_real_distribution<double> distribution;
-        std::uniform_real_distribution<double> sizeDistribution;
+
+        int sizeDistributeType=0;
+        double paramA;
+        double paramB;
+        double lmin;
+        enum{Uniform=0,Gaussian=1,exponential=2,weibull=3};  
+   
+
+      //  std::uniform_real_distribution<double> sizeDistribution;
         double _minSize;
 
         /**********************************************************************/
@@ -62,7 +72,11 @@ namespace model
         MicrostructureGenerator() :
         /* init list */ generator(std::chrono::system_clock::now().time_since_epoch().count()),
         /* init list */ distribution(0.0,1.0),
-        /* init list */ sizeDistribution(0.1,0.5),
+        /* init list */ sizeDistributeType(0),
+        /* init list */ paramA(0.0),
+        /* init list */ paramB(1.0),
+        /* init list */ lmin(1.0),
+       // /* init list */ sizeDistribution(0.1,0.5),
         /* init list */ _minSize(0)
         {
             int meshID(0);
@@ -70,7 +84,40 @@ namespace model
             bool use_boundary=false;
             EDR.readScalarInFile("./DDinput.txt","use_boundary",use_boundary);
             
+            EDR.readScalarInFile("./microstructureInput.txt","sizeDistributeType",sizeDistributeType);
             
+             switch (sizeDistributeType)
+		    {
+		       case Uniform:
+		           {
+		           //paraA is minimum value, paraB is maximum value. ranges:[0 1]
+            EDR.readScalarInFile("./microstructureInput.txt","unifA",paramA);
+            EDR.readScalarInFile("./microstructureInput.txt","unifB",paramB);
+		           }
+		            break;
+		       case Gaussian:
+		            //For normal distribution, paraA=mean value (normlized by sample size along x direction) range: (0 1)
+		            //                         paraB=standard deviation (normlized by mean value) range: >0
+		           {
+            EDR.readScalarInFile("./microstructureInput.txt","gaussA",paramA);
+            EDR.readScalarInFile("./microstructureInput.txt","gaussB",paramB);
+		           }
+		            break;
+		       case exponential:
+		            //P(x|paraA)=paraA*exp(-paraA*x)  generally generate lots of small source
+		           {
+            EDR.readScalarInFile("./microstructureInput.txt","expA",paramA);
+		           }
+		            break;
+		       case weibull:
+		            //P(x|paraA,paraB)=(paraA/paraB)*(x/paraB)^(paraA-1)*exp(-[x/paraB]^paraA)
+		            //paraA=shape parameter, paraB=scale parameter; generally generate lots of small source
+		           {
+            EDR.readScalarInFile("./microstructureInput.txt","weibA",paramA);
+            EDR.readScalarInFile("./microstructureInput.txt","weibB",paramB);
+		           }
+		            break;
+		    }
             
             
             if (use_boundary)
@@ -93,6 +140,8 @@ namespace model
             MatrixDimD C2Gtemp;
             EDR.readMatrixInFile("./DDinput.txt","C2G",C2Gtemp); // crystal-to-global orientation
             Material<Isotropic>::rotateCrystal(C2Gtemp);
+            EDR.readScalarInFile("./DDinput.txt","Lmin",lmin); // material by atomic number Z
+            
 
             
         }
@@ -113,7 +162,47 @@ namespace model
         /**********************************************************************/
         double randomSize()
         {
-            return _minSize*sizeDistribution(generator);
+            double randomvalue=0.0;
+            while (_minSize*randomvalue<2.1*lmin)
+            {
+				switch (sizeDistributeType)
+				{
+				   case Uniform:
+					   {
+					   //paraA is minimum value, paraB is maximum value. ranges:[0 1]
+						std::uniform_real_distribution<double> sizeDistribution(paramA,paramB);
+						randomvalue=sizeDistribution(generator);
+					   }
+						break;
+				   case Gaussian:
+						//For normal distribution, paraA=mean value (normlized by sample size along x direction) range: (0 1)
+						//                         paraB=standard deviation (normlized by mean value) range: >0
+					   {
+						std::normal_distribution<double> sizeDistribution(paramA,paramB*paramA);
+						randomvalue=sizeDistribution(generator);
+					   }
+						break;
+				   case exponential:
+						//P(x|paraA)=paraA*exp(-paraA*x)  generally generate lots of small source
+					   {
+						std::exponential_distribution<double> sizeDistribution(paramA);
+						randomvalue=sizeDistribution(generator);
+					   }
+						break;
+				   case weibull:
+						//P(x|paraA,paraB)=(paraA/paraB)*(x/paraB)^(paraA-1)*exp(-[x/paraB]^paraA)
+						//paraA=shape parameter, paraB=scale parameter; generally generate lots of small source
+					   {
+						std::weibull_distribution<double> sizeDistribution(paramA,paramB);
+						randomvalue=sizeDistribution(generator);
+					   }
+						break;
+				}
+            }
+ 
+            return _minSize*randomvalue;
+ 
+            
         }
         
     };
